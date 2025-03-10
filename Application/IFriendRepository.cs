8f4211ed322a79bc715df4a6679d29d9ef7f21e4
Gandoler <gl.krutoi@mail.ru>
2025-03-10 17:01:53 +0300

    надо будет подкрутить

--- Application/IFriendRepository.cs
+++ Application/IFriendRepository.cs
@@ -1,4 +1,3 @@
-using Entities.Models;
 using Entities.Templates;
 
 namespace UseCases;
--- Application/IUserRepository.cs
+++ Application/IUserRepository.cs
@@ -1,11 +1,11 @@
-using Entities.Models;
-
-namespace UseCases;
-
-public interface IUserRepository
-{
-    Task<User> UpdateAsync(User user);
-    Task<User> CreateAsync(User user);
-    Task<bool> ExicstCheckAsync(User user);
-
-}
+// using Entities.Models;
+//
+// namespace UseCases;
+//
+// public interface IUserRepository
+// {
+//     Task<User> UpdateAsync(User user);
+//     Task<User> CreateAsync(User user);
+//     Task<bool> ExicstCheckAsync(User user);
+//
+// }
