b7bf9c9ab1140bbed589d1acef626ab68157e9e2
Gandoler <gl.krutoi@mail.ru>
2025-03-12 11:47:25 +0300

    исправил ошибку дто

--- Domain.Interfaces/IServices/IAuthService.cs
+++ Domain.Interfaces/IServices/IAuthService.cs
@@ -4,5 +4,5 @@ namespace Domain.Interfaces.IServices;
 
 public interface IAuthService
 {
-    Task<bool> ExicstCheckAsync(CheckExistDto dto);
+    Task<bool> ExicstCheckAsync(LoginDto dto);
 }
--- Infrastructure.Buisiness/Services/AuthService.cs
+++ Infrastructure.Buisiness/Services/AuthService.cs
@@ -1,6 +1,23 @@
+using Domain.Interfaces;
+using Domain.Interfaces.IServices;
+using Domain.Models;
+using Entities.Templates;
+
 namespace UseCases.Services;
 
 public class AuthService:IAuthService
 {
-    
+    private readonly IUserRepository _userRepository;
+    private readonly IMailComprRepository _mailComprRepository;
+
+    public AuthService(IUserRepository userRepository, IMailComprRepository mailComprRepository)
+    {
+        _userRepository = userRepository;
+        _mailComprRepository = mailComprRepository;
+    }
+    public async Task<bool> ExicstCheckAsync(LoginDto dto)
+    {
+        
+        
+    }
 }
