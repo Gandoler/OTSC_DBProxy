6dc493870b7dd6550f9beb290438952ea0791173
Gandoler <gl.krutoi@mail.ru>
2025-03-18 15:28:11 +0300

    забыл про порт...

--- Domain.DTO/DTO/User/LoginDto.cs
+++ Domain.DTO/DTO/User/LoginDto.cs
@@ -2,8 +2,8 @@ namespace Entities.Templates;
 
 public class LoginDto
 {
-    public string Login { get; set; } = null!;
-    public string Password { get; set; } = null!;
+    public string Login { get; set; } =string.Empty;
+    public string Password { get; set; } = string.Empty!;
     
     
 }
