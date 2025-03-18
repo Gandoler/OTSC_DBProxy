dd7ade54044d5b1f6800be85d406c1089f680b03
Gandoler <gl.krutoi@mail.ru>
2025-03-18 13:06:01 +0300

    о

---
+++ Infrastructure.DATA/Migrations/20250318100314_Add ids to mail and tg comp.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addidstomailandtgcomp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_tg_comprehensions_appid",
+                table: "tg_comprehensions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_mail_comprehensions_appid",
+                table: "mail_comprehensions");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "tg_id",
+                table: "tg_comprehensions",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "tg_comprehensions_pkey",
+                table: "tg_comprehensions",
+                columns: new[] { "appid", "tg_id" });
+
+            migrationBuilder.AddPrimaryKey(
+                name: "mail_comprehensions_pkey",
+                table: "mail_comprehensions",
+                columns: new[] { "appid", "mail" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "tg_comprehensions_pkey",
+                table: "tg_comprehensions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "mail_comprehensions_pkey",
+                table: "mail_comprehensions");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "tg_id",
+                table: "tg_comprehensions",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tg_comprehensions_appid",
+                table: "tg_comprehensions",
+                column: "appid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_mail_comprehensions_appid",
+                table: "mail_comprehensions",
+                column: "appid");
+        }
+    }
+}
