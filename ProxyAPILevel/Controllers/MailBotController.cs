4fd32f7de8fd7b527bb68304ff0af9b097587b84
Gandoler <gl.krutoi@mail.ru>
2025-03-19 10:12:22 +0300

    добавил свагера для mail

--- ProxyAPILevel/Controllers/MailBotController.cs
+++ ProxyAPILevel/Controllers/MailBotController.cs
@@ -2,6 +2,7 @@ using Domain.DTO.DTO.Pozdr;
 using Domain.Interfaces.IServices;
 using Entities.Templates;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace ProxyAPILevel;
 
@@ -16,21 +17,39 @@ public class MailBotController : ControllerBase
         _mailBotService = mailBotService;
     }
 
+    /// <summary>
+    /// Получает список друзей с днями рождения сегодня.
+    /// </summary>
     [HttpGet("birthdays/today")]
+    [SwaggerOperation(Summary = "Получить сегодняшние дни рождения", Description = "Возвращает список друзей, у которых сегодня день рождения.")]
+    [SwaggerResponse(200, "Список успешно получен")]
     public async Task<IActionResult> GetTodayBirthdays()
     {
         var friends = await _mailBotService.SelectForTodayBithrday();
         return Ok(friends);
     }
 
+    /// <summary>
+    /// Получает строку поздравления по ID.
+    /// </summary>
     [HttpGet("pozdrik/{pozdrikId:int}")]
+    [SwaggerOperation(Summary = "Получить поздравление", Description = "Возвращает поздравительную строку по указанному ID Поздрика.")]
+    [SwaggerResponse(200, "Поздравление успешно найдено")]
+    [SwaggerResponse(404, "Поздравление не найдено")]
     public async Task<IActionResult> GetCongrStr(int pozdrikId)
     {
         var pozdrik = await _mailBotService.SelectPozdStringAsync(new PozdrikIdDto { _pozdrikId = pozdrikId });
         return pozdrik != null ? Ok(pozdrik) : NotFound(new { message = "Pozdrik not found" });
     }
 
+    /// <summary>
+    /// Получает email по AppId.
+    /// </summary>
     [HttpGet("email/{appId:guid}")]
+    [SwaggerOperation(Summary = "Получить email", Description = "Возвращает email по указанному AppId." +
+                                                                "\n\n(550e8400-e29b-41d4-a716-446655440000) - пример")]
+    [SwaggerResponse(200, "Email успешно найден")]
+    [SwaggerResponse(404, "Email не найден")]
     public async Task<IActionResult> GetEmail(Guid appId)
     {
         var email = await _mailBotService.GetEmailAsync(new AppIdDto { AppId = appId });
